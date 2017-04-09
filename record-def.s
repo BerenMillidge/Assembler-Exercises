174f4d9d104aea9ec5476cab5b1b77305d420524
Beren Millidge <bmillidgework@gmail.com>
2017-04-09 18:55:06 +0100

    helper function

---
+++ record-def.s
@@ -0,0 +1,7 @@
+#So this is the chapter 6 exercise in structured data, unlike standard files. We need these offsets to the above structure
+
+.equ RECORD_FIRSTNAME,0
+.equ RECORD_LASTNAME, 40
+.equ RECORD_ADDRESS, 80
+.equ RECORD_AGE, 320
+.equ RECORD_SIZE, 324
