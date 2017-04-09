724a46e70dcb4c2789b8baab92ae4d81e5177f7c
Beren Millidge <bmillidgework@gmail.com>
2017-04-09 18:55:18 +0100

    helper file

---
+++ linux.s
@@ -0,0 +1,21 @@
+#this is where we put our system call definitions, so we don't have to keep rewriting them everytimes
+
+#System call numbers
+.equ SYS_EXIT, 1
+.equ SYS_READ, 3
+.equ SYS_WRITE, 4
+.equ SYS_OPEN. 5
+.equ SYS_CLOSE, 6
+.equ SYS_BRK, 45
+.equ SYS_CREATE_OPEN, 0101
+
+#System call interrupt number
+.equ LINUX_SYSCALL, 0x80
+
+#Standard file descriptors
+.equ STDIN, 0
+.equ STDOUT, 1
+.equ STDERR, 2
+
+#Common status codes
+.equ END_OF_FILE, 0
